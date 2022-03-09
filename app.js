d69685887ffdcc0ff8cf01a33b53a2a0616d13f6
swapnilaryan97 <saiamthere@gmail.com>
2022-03-09 11:54:40 -0500

    created flipCard and checkMatch functions

--- app.js
+++ app.js
@@ -53,15 +53,36 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const grid = document.querySelector('#grid')
-console.log(grid)
+const cardChosen = []
 
 function createBoard() {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         grid.append(card)
     }
 }
 
 createBoard()
+
+function flipCard() {
+    let cardID = this.getAttribute('data-id')
+    cardChosen.push(cardArray[cardID].name)
+    // console.log('clicked', cardID)
+    // console.log(cardChosen)
+    this.setAttribute('src', cardArray[cardID].img)
+
+    if (cardChosen.length ===2) {
+        setTimeout(checkMatch, 500)
+    }
+}
+
+function checkMatch(){
+    console.log('check for match')
+    if (cardChosen[0] === cardChosen[1]) {
+        alert('You found a match!')
+    }
+}
+
