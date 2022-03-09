0badab4d1033ca6f30fce6059760f922b447c970
swapnilaryan97 <saiamthere@gmail.com>
2022-03-09 00:49:00 -0500

    created board in app.js

--- app.js
+++ app.js
@@ -1,8 +1,67 @@
 const cardArray = [
-   {
-    name: 'fries',
-    
-   }
-
-
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    }
 ]
+
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const grid = document.querySelector('#grid')
+console.log(grid)
+
+function createBoard() {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        grid.append(card)
+    }
+}
+
+createBoard()
